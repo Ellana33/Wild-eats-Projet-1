9f1072b85d9a1e2a6925504e099cd7c6fdc9f8f0
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-09-27 12:11:57 +0200

    [ADD] all list of restaurants

--- restaurant.js
+++ restaurant.js
@@ -3,38 +3,115 @@ const restaurants = document.querySelector(".grid");
 const listOfRestaurants = [
     {
         name: "Flam's",
-        picture: "img/filtre.png",
+        picture: "img/restaurants/flams.jpg",
         type: "Flammekuche",
         note: "4.7",
         price: "$"
     },
     {
         name: "McDonald's",
-        picture: "https://placekitten.com/200/139",
+        picture: "img/restaurants/mcdo.jpg",
         type: "Sur le pouce",
         note: "3.4",
         price: "$",
     },
     {
         name: "Bistrot régent",
-        picture: "https://placekitten.com/200/90",
+        picture: "img/restaurants/bistrot regent.jpg",
         type: "Français",
         note: "4.1",
         price: "$$",
     },
     {
         name: "Eat Salad",
-        picture: "https://placekitten.com/200/140",
+        picture: "img/restaurants/eat salad.jpg",
         type: "Salade",
         note: "4.2",
         price: "$",
     },
     {
         name: "Le foodtruck bio",
-        picture: "https://placekitten.com/200/141",
+        picture: "img/restaurants/foodtruck.jpg",
         type: "Restauration rapide",
         note: "3.8",
         price: "$",
+    },
+    {
+        name: "Le bureau",
+        picture: "img/restaurants/au bureau.jpg",
+        type: "Bistrot",
+        note: "4.3",
+        price: "$$",
+    },
+    {
+        name: "Papadum",
+        picture: "img/restaurants/papadum.jpg",
+        type: "Indien",
+        note: "4.7",
+        price: "$",
+    },
+    {
+        name: "Bistrot Spinoza",
+        picture: "img/restaurants/spinoza.png",
+        type: "Bistrot",
+        note: "4.7",
+        price: "$$",
+    },
+    {
+        name: "Su and Shi",
+        picture: "img/restaurants/sushi.jpg",
+        type: "Japonais",
+        note: "4.1",
+        price: "$$",
+    },
+    {
+        name: "La dame",
+        picture: "img/restaurants/la dame.png",
+        type: "Nocturne",
+        note: "3.2",
+        price: "$",
+    },
+    {
+        name: "Joya",
+        picture: "img/restaurants/joya.jpg",
+        type: "Nocturne",
+        note: "3.5",
+        price: "$$",
+    },
+    {
+        name: "La côte et l'Arète",
+        picture: "img/restaurants/cote arete.jpg",
+        type: "Français",
+        note: "4.5",
+        price: "$$",
+    },
+    {
+        name: "La terrasse",
+        picture: "img/restaurants/la terasse.jpg",
+        type: "Français",
+        note: "4.3",
+        price: "$$",
+    },
+    {
+        name: "Tripletta",
+        picture: "img/restaurants/tripletta.jpg",
+        type: "Italien",
+        note: "4.4",
+        price: "$$",
+    },
+    {
+        name: "Le Jardin Pêcheur",
+        picture: "img/restaurants/le jardin pecheur.jpg",
+        type: "Brasserie",
+        note: "4.1",
+        price: "$$",
+    },
+    {
+        name: "Café Maritime",
+        picture: "img/restaurants/cafe maritime.jpg",
+        type: "Français",
+        note: "4.0",
+        price: "$$",
     }
 ];
 
@@ -44,7 +121,7 @@ function createRestaurants (title, imageUrl, type, reward, price) {
     restaurants.appendChild(restaurant);
 
     const restaurantPicture = document.createElement("img");
-    restaurantPicture.src = imageUrl;
+    restaurantPicture.src = `${imageUrl}`;
     restaurantPicture.classList.add("restaurant-card-img");
     restaurant.appendChild(restaurantPicture);
 
