c9bc46b549a02d22f03dd3f6cdc2db46be1ce337
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-10-03 14:45:12 +0200

    [ADD] more feedback about sur le pouce

--- restaurants.js
+++ restaurants.js
@@ -7,9 +7,9 @@ const listOfRestaurants = [
         price: "$",
         description: "Une trattoria ancrée dans la tradition est généralement dépourvue de menu imprimé, avec un service décontracté, du vin vendu à la carafe plutôt qu'à la bouteille, des prix bas et un menu d'offres modestes mais abondantes qui suivent des recettes régionales et locales plutôt que de la haute cuisine.",
         map: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d45249.98516144877!2d-0.5863512414017767!3d44.85976888757434!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd552985c9603b43%3A0x8950156090451f57!2sIT%20-%20Italian%20Trattoria%20-%20Bassins%20%C3%A0%20Flot%20N%C2%B02%2C%2040%20quai%20Virginie%20H%C3%A9riot%2C%20Bordeaux!5e0!3m2!1sen!2sfr!4v1696327610281!5m2!1sen!2sfr",
-        feedback: ["Fontanille Nina : \"Un des meilleurs restaurant italien à Bordeaux. \n Habitant à 45 min de votre restaurant, nous sommes à chaque fois impatient de venir y déguster vos plats ... \"",
+        feedback: ["Fontanille Nina : \"Un des meilleurs restaurant italien à Bordeaux. \n Habitant à 45 min de votre restaurant, nous sommes à chaque fois impatient de venir y déguster vos plats ...\"",
                     "FZ EA : \"Bonne cuisine italienne, même si nos plats avaient certains bémols(les pâtes a la truffe manque de goût de truffe et les pâtes aux anchois trop salées.) Les désserts ont rattrapés le repas car très bons (tiramisu sans alcool). Le restaurat propose des portions XL pour les plus gourmand ! ...\"",
-                    "Carine Jansana : \" En sortant de la visite des bassins des lumières, à 14h, superbe déjeuner tardif (aucun soucis pour l'arrivée tardive) Accueil sympatique, choix d'une pizza vegetale, de pasta amalfi et de pasta modena : les trois sont succulent et copieux. ... \""]
+                    "Carine Jansana : \"En sortant de la visite des bassins des lumières, à 14h, superbe déjeuner tardif (aucun soucis pour l'arrivée tardive) Accueil sympatique, choix d'une pizza vegetale, de pasta amalfi et de pasta modena : les trois sont succulent et copieux. ...\""]
     },
     {
         name: "Flam's",
@@ -33,7 +33,9 @@ const listOfRestaurants = [
         filterType: "Sur le pouce",
         description: "Chaîne classique de restauration rapide établie de longue date connue pour ses burgers et ses frites.",
         map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2827.619432866565!2d-0.5675503237582161!3d44.870041771070454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd55285fa11b5623%3A0x804453834483b6bd!2sMcDonald&#39;s%20Port%20Autonome!5e0!3m2!1sfr!2sfr!4v1695993503644!5m2!1sfr!2sfr",
-        feedback: ["Marion Nouvite : \"Accueil très chaleureux, lieu très propre, nouvelle vaisselle écologique, ballond animaliers offerts aux enfants ainsi que des sachets de bonbons! Du jamais vu!\""],
+        feedback: ["Marion Nouvite : \"Accueil très chaleureux, lieu très propre, nouvelle vaisselle écologique, ballond animaliers offerts aux enfants ainsi que des sachets de bonbons! Du jamais vu!\"",
+        "Julien Ternette : \"L'équipière du McDo, Isabelle, à ete absolument gentille et agréable lorsque je suis venu déjeuner ce midi. Service impeccable, prise de commande à la caisse pour me faire gagner du temps, rapide, et puis tout simplementfort aimable !\"",
+        "Malika Siette : \"Merci à Isabelle pour cet après-midi d'anniversaire. Mon fils ainsi que ses copains ont un super moment pour ses 5 ans. Le seul hic dommage ont voit que le temps c'est de l'argent...\""],
     },
     {
         name: "Bistrot régent",
@@ -61,7 +63,8 @@ const listOfRestaurants = [
         map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1887.4110264353674!2d-0.5597578414046314!3d44.863897760445205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd55288c3ebd5fd3%3A0xca0fc2319444168e!2sEat%20Salad!5e0!3m2!1sfr!2sfr!4v1695993849704!5m2!1sfr!2sfr",
         feedback:
         ["Véronique Frémond-Kahlouche : \"Très agréable restaurant où l'accueil est particulièrement chalereux ! On y mange de très bonnes choses de grande qualité et fraîcheur ! J'adore y aller ! Je recommande chaudement.\"",
-        "Christian Delégume : \"Une équipe jeune, sympathique et accueillante. Le concept est simple et redoutablement efficace. Pour ne rien gâcher c'est hyper bon. Si vous hésitez, vous avez tort. Allez-y et régalez vous. C'est aussi simple que cela.\""],
+        "Christian Delégume : \"Une équipe jeune, sympathique et accueillante. Le concept est simple et redoutablement efficace. Pour ne rien gâcher c'est hyper bon. Si vous hésitez, vous avez tort. Allez-y et régalez vous. C'est aussi simple que cela.\"",
+        "Aurore Movie : \"Très bien! La serveuse était agréable et patiente, et la nourriture fraîche et savoureuse!\""],
     },
     {
         name: "Le foodtruck bio",
@@ -74,7 +77,9 @@ const listOfRestaurants = [
         description: "Le foodtruck bio propose des crêpes et des galettes préparées avec des ingrédients biologiques de qualité. C'est une option de restauration rapide saine et respectueuse de l'environnement.",
         map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2827.5997177715944!2d-0.5625698230891266!3d44.87044307107046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5529ec2456ab0b%3A0x69bdfff0571c7115!2sLe%20foodtruck%20bio!5e0!3m2!1sfr!2sfr!4v1695994245877!5m2!1sfr!2sfr",
         feedback: ["Noa de Cajou : \"Très bon moment de détente après la visite de la base sous-marine. \
-        Crêpe délicieuse et bière de la région bordelaise.\""],
+        Crêpe délicieuse et bière de la région bordelaise.\"",
+        "Silvano Ptique : \"Bonne adresse pour déguster des crêpes et des galettes avec du fromage ou des douceurs. Agréable moment de détente et de restauration après la visite de la lumière du bassin.\"",
+        "Jean Saigne : \"Joli kiosque avec des produits bio, un moment de détente en mangeant une crêpe ou des galettes bio avant ou après le musée. Des gens jeunes et gentils.\""],
     },
     {
         name: "Le bureau",
@@ -237,8 +242,10 @@ const createPage = (index) => {
             <div class="text-restaurant">
                 <h3 class="title-restaurant">${listOfRestaurants[index].name}</h3>
                 <p class="type-food">${listOfRestaurants[index].type}</p>
+            <div class="note-price">
                 <span class="star">${listOfRestaurants[index].note}</span>
                 <span class="price">${listOfRestaurants[index].price}</span>
+            </div>
                 <p class="description">${listOfRestaurants[index].description}</p>
                 <div class="location">
                 <iframe src="${listOfRestaurants[index].map}" width="250" height="112" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -248,6 +255,7 @@ const createPage = (index) => {
                 <div class="feedback-cards">
                     <p class="feedback"><span class="yellow-stars">*** </span>${listOfRestaurants[index].feedback[0]}</p>
                     <p class="feedback"><span class="yellow-stars">*** </span>${listOfRestaurants[index].feedback[1]}</p>
+                    <p class="feedback"><span class="yellow-stars">*** </span>${listOfRestaurants[index].feedback[2]}</p>
                 </div>
             </div>
         `;
