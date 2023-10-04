b99252aa42387a876e6309a6f932083fb1baabeb
Nassime HARMACH <Nassitch@hotmail.fr>
2023-10-04 15:28:26 +0200

    Use Prettier

--- functions/writeAReview.js
+++ functions/writeAReview.js
@@ -1,4 +1,3 @@
-
 // Déclaration de la variable "formReview" et assignation à l'ID "write-review".
 const formReview = document.querySelector("#write-review");
 
@@ -15,39 +14,37 @@ const approuvPop = document.querySelector("#approuvMsg");
 // Ajout de la variable "inputs" qui est un tableau contenant "userReview" et "contentReview"  ansi que les étoiles.
 let inputs = [userReview, contentReview, starReview];
 
-
 // Verification du contenu des champs et affichage d'erreur en cas d'erreurs.
-submitReview.addEventListener("click", (send)=> {
-    send.preventDefault();
-    
-    if (userReview.value === "") {
-        errorPop.innerHTML = "Votre Prénom/Nom n'est pas correctement tapé.";
-        errorPop.classList.add("show");
-        } else if (contentReview.value === "") {
-                errorPop.innerHTML = "Votre avis rédigé n'est pas valide.";
-                errorPop.classList.add("show");
-        } else {
-            approuvPop.classList.add("show");
-            approuvPop.innerHTML = "Le commentaire est ajouté avec succès.";
-            errorPop.classList.remove("show");
-            
-            // Block d'instructions pour push le commentaire.
-            
-                const feedbackBlock = document.querySelector(".feedback-content");
-                userReview = document.createElement("span");
-                contentReview = document.createElement("p");
-                
-                userReview.classList.add("yellow-stars");
-                contentReview.classList.add("feedback");
-                
-                userReview.innerHTML = inputs[2].value + " " + inputs[0].value + " :";
-                contentReview.innerHTML = inputs[1].value;
-                
-                feedbackBlock.appendChild(userReview);
-                feedbackBlock.appendChild(contentReview);
-                
-                inputs[0].value = "";
-                inputs[1].value = "";
-        }
-});
+submitReview.addEventListener("click", (send) => {
+  send.preventDefault();
 
+  if (userReview.value === "") {
+    errorPop.innerHTML = "Votre Prénom/Nom n'est pas correctement tapé.";
+    errorPop.classList.add("show");
+  } else if (contentReview.value === "") {
+    errorPop.innerHTML = "Votre avis rédigé n'est pas valide.";
+    errorPop.classList.add("show");
+  } else {
+    approuvPop.classList.add("show");
+    approuvPop.innerHTML = "Le commentaire est ajouté avec succès.";
+    errorPop.classList.remove("show");
+
+    // Block d'instructions pour push le commentaire.
+
+    const feedbackBlock = document.querySelector(".feedback-content");
+    userReview = document.createElement("span");
+    contentReview = document.createElement("p");
+
+    userReview.classList.add("yellow-stars");
+    contentReview.classList.add("feedback");
+
+    userReview.innerHTML = inputs[2].value + " " + inputs[0].value + " :";
+    contentReview.innerHTML = inputs[1].value;
+
+    feedbackBlock.appendChild(userReview);
+    feedbackBlock.appendChild(contentReview);
+
+    inputs[0].value = "";
+    inputs[1].value = "";
+  }
+});
