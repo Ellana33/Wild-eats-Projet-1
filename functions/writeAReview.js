60d7bbbe0721431b173be6d1b4b3f77586d6d614
Laurore83 <laurore83@yahoo.fr>
2023-10-05 11:58:45 +0200

    storage

--- functions/writeAReview.js
+++ functions/writeAReview.js
@@ -26,6 +26,14 @@ submitReview.addEventListener("click",  function writeAReviewContent(send) {
     errorPop.innerHTML = "Votre avis rédigé n'est pas valide.";
     errorPop.classList.add("show");
   } else {
+
+    let myReview = {
+      userReview: userReview.value,
+      contentReview: contentReview.value,
+      starReview: starReview.value
+    }
+    localStorage.setItem("userReview", myReview);
+    console.log(myReview);
     approuvPop.classList.add("show");
     approuvPop.innerHTML = "Le commentaire est ajouté avec succès.";
     errorPop.classList.remove("show");
